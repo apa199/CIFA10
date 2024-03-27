caa7a3cb846ce179cb6d43aefe4b48784c012cc9
apa199 <160258729+apa199@users.noreply.github.com>
2024-03-27 23:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
-import streamlit as st
-import tensorflow as tf
-# st.set_option('deprication.showfileUploderEncoding',False)
-# st.set_option('deprecation.showfileUploaderEncoding', False)
-@st.cache(allow_output_mutation=True)
-
-def load_model():
-    model=tf.keras.models.load_model('my_model.hdf5')
-    return model
-model=load_model()
-st.write(""" image classification """)
-file=st.file_uploader("please upload your file",type=['jpg','png'])
+import os
 import cv2
-from PIL import Image, ImageOps
-import numpy as np
-def import_and_predict(Image_data,model):
-    size=(32,32,3)
-    image=ImageOps(Image_data, size)
-    img=np.arrayshape(image)
-    img.reshape= img[32,32,3]
-    prediction=model.predict(img.reshape)
+import numpy as np 
+from PIL import Image , ImageOps
+import streamlit as st
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+
+class_name = ["airplane", "automobile" , "bird" , "cat" , "deer" , "dog" , "frog" , "horse" , "ship" , "truck"]
+
+# Create a function to load my saved model
+@st.cache(allow_output_mutation=True)
+def load_my_model():
+    model = tf.keras.models.load_model("my_model.hdf5")
+    return model
+
+model = load_my_model()
+
+# Create a title of web App
+st.title("Image Classification with Cifar10 Dataset")
+st.header("Please Upload images related to this things...")
+st.text(class_name)
+
+# create a file uploader and take a image as an jpg or png
+file = st.file_uploader("Upload the image" , type=["jpg" , "png"])
+
+# Create a function to take and image and predict the class
+def import_and_predict(image_data , model):
+    size = (32 ,32)
+    image = ImageOps.fit(image_data , size)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
     return prediction
-if file is None:
-    st.text("please upload a pic")
-else:
-    image=Image.open(file)
-    model=tf.keras.models.load_model('my_model.hdf5')
-    st.image(image, use_column_width=True)
-    preditions=import_and_predict(image,model)
-    class_name=["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"]
-    string=["this is most likey :"+class_names[np.argmax(predictions)]]
+
+if st.button("Predict"):
+    image = Image.open(file)
+    st.image(image , use_column_width=True)
+    predictions = import_and_predict(image , model)
+
+    class_name = ["airplane", "automobile" , "bird" , "cat" , "deer" , "dog" , "frog" , "horse" , "ship" , "truck"]
+
+    string = "Image mostly same as :-" + class_name[np.argmax(predictions)]
     st.success(string)
